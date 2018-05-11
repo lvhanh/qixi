db220703517e4e28fe7c5e11fc6e02789b816582
lvhanh <36942284+lvhanh@users.noreply.github.com>
2018-05-11 17:00:22 +0800

    Add files via upload

--- js/QiXi.js
+++ js/QiXi.js
@@ -1,4 +1,24 @@
 /*
+*开灯效果
+*/
+var lamp = {
+	ele : $(".b_background"),
+	bright : function(){
+		this.ele.addClass("lamp-bright");
+	},
+	dark : function(){
+		this.ele.removeClass("lamp-bright");
+	}
+};
+//开灯
+var bright = function(){
+	return lamp.bright();
+}
+//关灯
+var dark = function(){
+	return lamp.dark();
+}
+/*
 *开门效果
 */
 function doorAction(time,left,right){
@@ -20,28 +40,19 @@ function doorAction(time,left,right){
 	},time,complete());
 	return dtd;
 }
+//开门
 var openDoor = function(){
 	return doorAction(2000,'-50%','100%');
 }
+//关门
 var closeDoor = function () {
 	return doorAction(2000,'0%','50%');
 }
-/*
-*开灯效果
-*/
-var lamp = {
-	ele : $(".b_background"),
-	bright : function(){
-		this.ele.addClass("lamp-bright")
-	},
-	dark : function(){
-		this.ele.removeClass("lamp-bright")
-	}
-};
 
 /*
 *小孩走路
 */
+var boyTOdoor;
 function boyWalk(){
 	var swip = Swip($("#content"));
 	/*var backgroundCss = function(className){
@@ -62,6 +73,7 @@ function boyWalk(){
 	// 页面可视区域宽高
 	var visualWidth = $("#content").width();
 	var visualHeight = $("#content").height();
+
 
 	// 慢走
 	function slowWalk(){
@@ -107,6 +119,52 @@ function boyWalk(){
 		},time);
 		return dl;
 	}
+
+	//走进商店
+	function walkToShop(time){
+		var offsetDoor = $(".door").offset();
+		var offsetDoorLeft = offsetDoor.left;
+		var offsetBoy = $boy.offset();
+		var offsetBoyLeft = offsetBoy.left;
+		boyTOdoor = (offsetDoorLeft + $(".door").width()/2) - (offsetBoyLeft + $boy.width()/2);
+		var defer = $.Deferred();
+		var walkPlay = startRun({
+			transform : 'translateX('+boyTOdoor+'px),scale(0.3,0.3)',
+			opacity : 0.1
+		},time);
+		walkPlay.done(function(){
+			$boy.css({
+				opacity : 0
+			});
+			defer.resolve();
+		});
+		return defer;
+	}
+
+	//走出商店
+	function walkOutShop(time){
+		var defer = $.Deferred(); 
+		var walkPlay = startRun({
+			transform : 'translateX('+boyTOdoor+'px),scale(1,1)',
+			opacity : 1
+		},time);
+		walkPlay.done(function(){
+			defer.resolve();
+		});
+		return defer;
+	}
+
+	//取花
+	function takeFlower(){
+		var defer = $.Deferred();
+		//设置延时
+		setTimeout(function(){
+			$boy.addClass("takeFlower");
+			defer.resolve();
+		},1000);
+		return defer;
+	}
+
 	return {
 		// 开始走路
 		walkTo : function(time,proportionX,proportionY){
@@ -118,9 +176,21 @@ function boyWalk(){
 		stopWalk : function(){
 			pauseWalk();
 		},
+		//走进商店
+		walktoShop : function(time){
+			return walkToShop(time);
+		},
+		//走出商店
+		walkoutShop : function(time){
+			return walkOutShop(time);
+		},
 		//改变颜色
 		setColor : function(val){
 			$boy.css('background-color',val);
+		}
+		//取花
+		takeFlower : function(){
+			return takeFlower();
 		}
 	};
 }
